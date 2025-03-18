0293cec03abe6154d68630adda0c8d726b36c61c
Khoi Phan <jasonphan2631@gmail.com>
2025-03-17 20:52:55 -0700

    121

--- src/UI/CardUI.jsx
+++ src/UI/CardUI.jsx
@@ -3,7 +3,7 @@ export default function CardUI(props) {
   const { cardArray } = props;
 
   return (
-    <div style={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)', gap: '50px' }}>
+    <div className="card-ui-main" style={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)', gap: '50px' }}>
       {cardArray.map((card, index) => (
       <div key={index} style={{ border: '3px solid #ccc', padding: '12px', borderRadius: '10px' }}>
         <h3>{card.skillName}</h3>
--- src/UI/NavTabs.jsx
+++ src/UI/NavTabs.jsx
@@ -8,7 +8,7 @@ export default function NavTabs({ links }) {
     <nav className="navbar navbar-expand-lg bg-primary">
       <div className="container-fluid">
 
-          <ul className="nav nav-tabs navbar-nav me-auto mb-2 mb-lg-0">
+          <ul className="nav-list nav nav-tabs navbar-nav me-auto mb-2 mb-lg-0">
             {links.map((link) => link)}
           </ul>
         </div>
